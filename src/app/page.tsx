7b5d8be1999e76ca3152ab813981996b498d92b6
David Barbosa <vid.barbosa@gmail.com>
2024-03-29 17:35:02 -0500

    Fix formatting in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="absolute inset-0 bg-gradient-to-t from-gray-200 to-gray-300 h-screen bg-cover -z-50">
-      <div className="pt-40   px-10">
+      <div className="pt-40 px-10">
         Home
 
       </div>
